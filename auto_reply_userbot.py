c9bafe29f918d0ea9206e3015d13cc9592bd6081
Danil Rastyapin <danil.rastyapin@gmail.com>
2025-05-04 04:28:14 +0300

    update of reply service on 'hello world' and fixing .env. AND RUDIMENTATION OF CONFIG

--- auto_reply_userbot.py
+++ auto_reply_userbot.py
@@ -2,11 +2,11 @@ from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 import os, logging
 
-API_ID   = int(os.getenv("TG_API_ID"))
-API_HASH = os.getenv("TG_API_HASH")
-TARGET   = os.getenv("TG_TARGET")        # пишем @durov чтобы свести его с ума 
-REPLY    = os.getenv("TG_REPLY_TEXT")
-SESSION  = os.getenv("TG_SESSION")       # сессия
+API_ID   = int(os.environ["TG_API_ID"])
+API_HASH = os.environ["TG_API_HASH"]
+SESSION  = os.environ["TG_SESSION"]
+TARGET   = os.environ["TG_TARGET"]
+REPLY    = os.getenv("TG_REPLY_TEXT", "hello world!")
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
 
@@ -14,10 +14,10 @@ client = TelegramClient(StringSession(SESSION), API_ID, API_HASH)
 
 @client.on(events.NewMessage(chats=TARGET))
 async def on_post(ev):
-    if ev.is_channel and not ev.is_group:
+    if ev.is_channel:
         await client.send_message(ev.chat_id, REPLY, comment_to=ev.id)
-        logging.info("Replied to post %s", ev.id)
+        logging.info("commented post %s", ev.id)
 
 with client:
-    logging.info("Userbot online. Waiting for posts…")
+    logging.info("Userbot ONLINE, waiting for posts…")
     client.run_until_disconnected()
